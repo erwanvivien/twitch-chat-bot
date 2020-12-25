cf082ac43b5eecbb99b8777613b260cbd086cca5
erwan.vivien <erwan.vivien@epita.fr>
2020-12-25 20:55:03 +0100

    clean-up: removed two old files

--- src/twitch.py
+++
@@ -1,7 +0,0 @@
-from utils import get_content
-
-
-# [:-1] trims last \n
-client_id, client_secret = get_content("client_id")[:-1], get_content("client_secret")[:-1]
-
-twitch_header = {"client-id": client_id, "Authorization": f"Bearer {client_secret}"}

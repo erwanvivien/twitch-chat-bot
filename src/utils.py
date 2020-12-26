55bef4576d2974b6002d99d9caecd6736a78332b
erwan.vivien <erwan.vivien@epita.fr>
2020-12-26 11:38:41 +0100

    comments: added many comments for default functions in utils

--- src/utils.py
+++ src/utils.py
@@ -6,23 +6,28 @@ DEBUG_MSG = False
 DEBUG_FCT = True
 DEBUG_VALUES = False
 
-# If file 'bruh' contains "Hellor"
-# get_content("bruh") returns 'Hellor'
-
 
 def get_content(file):
+    """Returns a string
+
+    If file 'bruh' contains "Hellor"
+    get_content("bruh") returns 'Hellor'
+    """
+
     file = open(file, "r")
     res = file.read()
     file.close()
     return res
 
-# :xiaojiba!xiaojiba@xiaojiba.tmi.twitch.tv PRIVMSG #xiaojiba :test
-# Extracts "test"
-# :xiaojiba!xiaojiba@xiaojiba.tmi.twitch.tv PRIVMSG #xiaojiba :salur : test
-# Extracts "salur : test"
-
 
 def get_message_content(ircstring=None):
+    """Returns a string
+
+    from => ":xiaojiba!xiaojiba@xiaojiba.tmi.twitch.tv PRIVMSG #xiaojiba :test"
+    Extracts "test"
+    from => ":xiaojiba!xiaojiba@xiaojiba.tmi.twitch.tv PRIVMSG #xiaojiba :salur : test"
+    Extracts "salur : test"
+    """
     if not ircstring:
         return ""
 
@@ -38,6 +43,8 @@ def get_message_content(ircstring=None):
     return s[:-2]
 
 
+# Change every possibility that you want to handle
+# CAPS only but leave at least 'VOID' (the default one, does nothing)
 possibilities = {
     "VOID": [0, game.void],
 
@@ -57,8 +64,12 @@ possibilities = {
 
 
 def reset_current():
-    # Every possible word to use in the chat
-    # [0] is the current key count - [1] is the associated function
+    """Returns a dictionnary
 
-    # resets the current to default
+    With very possible word to use in the chat
+    [0] is the current key count - [1] is the associated function
+
+    resets the current to default
+    """
+
     return copy.deepcopy(possibilities)
